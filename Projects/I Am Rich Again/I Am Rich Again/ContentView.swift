b1ce4ed082a1d6647e8784471b1974012491804d
Middi <richard@richardmiddleton.me>
2021-01-10 20:16:34 +0000

    i am rich swift ui app

---
+++ Projects/I Am Rich Again/I Am Rich Again/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  I Am Rich Again
+//
+//  Created by Richard Middleton on 10/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich!")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Image("diamond").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone X"))
+    }
+}
